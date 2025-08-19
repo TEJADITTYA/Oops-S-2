de0eaa7f2662b19b83257401f27bf417eaaf6c16
TEJADITTYA <tejadityap@gmail.com>
2025-08-19 14:00:57 +0530

    Add files via upload
    Signed-off-by: TEJADITTYA <tejadityap@gmail.com>

---
+++ Finalvar.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Finalvar 
+{
+
+public static void main(String args[])
+    {
+	int x=10;
+	int y=20;
+	System.out.println("x is:"+x);
+	System.out.println("y is:"+y);
+
+	x=30;
+	y=40;
+	System.out.println("x is:"+x);
+	System.out.println("y is:"+y);
+    }
+}
+
